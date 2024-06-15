e64ac9804d33872c951f0798b116f841cb872f72
musikfed <obzorniky@gmail.com>
2024-06-15 15:44:09 +0300

    Update greeting message

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, CI/CD Pipeline!")
+	})
+
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
+}
+
+func Add(a, b int) int {
+	return a + b
+}
