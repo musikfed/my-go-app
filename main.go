de025eb22310befeda624f8b744999ee5df80dc1
musikfed <obzorniky@gmail.com>
2024-06-16 22:36:31 +0300

    https://github.com/musikfed/my-go-app.git

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, CI/CD Pipeline!11")
+		fmt.Fprintln(w, "Hello, CI/CD Pipeline!11111111")
 	})
 
 	fmt.Println("Starting server on :8080")
